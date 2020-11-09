daa7bedee329a910441cdc1fffe59c39b40eeb07
jbelow <joshbelow@gmail.com>
2020-11-09 09:45:22 -0600

    Initial commit

---
+++ Mode/Categories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindConsole.Mode
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "You didn't enter in a category name!")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
